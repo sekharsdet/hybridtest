7d5e4b5e0746b946671a7324b5d5cb1c5c4b55f1
sekharsdet <qesekhar546@gmail.com>
2022-06-13 18:00:08 +0200

    updated readme.md

--- cypress/e2e/pages/addNewAdvertisemnet.page.js
+++ cypress/e2e/pages/addNewAdvertisemnet.page.js
@@ -7,10 +7,6 @@ module.exports = {
         cy.get('input[name="name"]').clear().type(text);
     },
 
-    clearName: () => {
-        cy.get('input[name="name"]').clear();
-    },
-
     enterStreet: (text) => {
         cy.get('input[name="street"]').clear().type(text);
     },
@@ -23,31 +19,11 @@ module.exports = {
         cy.get('input[name="price"]').clear().type(text);
     },
 
-    clearPrice: () => {
-        cy.get('input[name="price"]').clear();
-    },
-
     selectStatus: (text) => {
         cy.get('md-checkbox>div.md-container').click({ force: true })
     },
 
     clickSaveButton: () => {
         cy.contains('save').click();
-    },
-
-    clickCancelButton: () => {
-        cy.contains('cancel').click();
-    },
-
-    verifySaveButton: () => {
-        cy.contains('save').should('be.disabled');
-    },
-
-    verifyErrorMsg: (msg) => {
-        cy.xpath('//div[@role="alert"]').should('contain', msg);
-    },
-
-    clickOKButtonInConfirmation: () => {
-        cy.contains('ok').click();
     }
 }
